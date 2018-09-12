0581bf5b6235727b5b3d09d264e08976686cc721
Harmandeep Singh <deepharman2000@gmail.com>
2018-09-12 17:23:24 -0400

    added a file

---
+++ lab3/src/lab3/Assignment.java
@@ -0,0 +1,8 @@
+package lab3;
+
+public class Assignment {
+	public static void main(String[]args) {
+		System.out.println("Kidan");
+	}
+
+}
---
+++ lab54/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author manusingh
+ *
+ */
+module lab54 {
+}
