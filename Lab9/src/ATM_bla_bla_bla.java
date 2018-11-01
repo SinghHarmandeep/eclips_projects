136105ac82988d4e2bd078a424c23ccec5bdd05e
Harmandeep Singh <deepharman2000@gmail.com>
2018-10-31 20:22:24 -0400

    Working Bank Account/ATM Machine

---
+++ Lab9/src/ATM_bla_bla_bla.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class ATM_bla_bla_bla {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Account[] accounts = new Account[10];
+	
+		for(int i = 0; i < accounts.length; i++) {
+			Account noOfAccounts = new Account(i, 100,4);
+			accounts[i]=noOfAccounts;
+		}
+		int input_id;
+		int choice;
+		
+		while(5>3) {	
+			System.out.println("Enter an id between 0 to 9 for more information about that id: ");
+			input_id = scan.nextInt();
+		while(input_id<0 || input_id>9) {
+			System.out.println("Incorrect Input.\nEnter an id between 0 to 9 ");
+			input_id = scan.nextInt();
+			}
+			
+		do {
+			System.out.println("\nMain menu: \n1:Check Balance \n2:Withdraw \n3:Deposit \n4:Exit");
+			System.out.println("Enter your choice: ");
+			choice = scan.nextInt();
+			
+			switch (choice) {
+			case 1:
+				System.out.println("Your account balance is: " +accounts[input_id].getBalance() + "\n\n");
+				break;
+			case 2:
+				System.out.println("Enter the amount you want to withdraw");
+				accounts[input_id].withdraw(scan.nextDouble());
+				break;
+			case 3:
+				System.out.println("Enter the amount you wish to deposit");
+				accounts[input_id].deposit(scan.nextDouble());
+				break;
+			
+			}
+			
+			
+		} while (choice !=4);
+	}
+		
+		
+		
+		
+		
+		
+	}
+}
+
---
+++ Lab9/src/Account.java
@@ -0,0 +1,82 @@
+import java.time.LocalDate;
+
+/**
+ * 
+ * @author manusingh
+ *<p>Mahadavi's Homework Assignment part 1
+ */
+public class Account {
+	private int id;
+	private double balance; //max 100
+	private double anualInterestRate;
+	LocalDate dateCreated = LocalDate.now(); //the day the account was created
+	
+	//constructors assignments
+	public Account(int _id, double _balance, double anualInterest) {
+		this.setId(_id);
+		this.setBalance(_balance);
+		this.setAnualInterestRate(anualInterest);
+	}
+	public Account(int _id, double _balance) {
+		this.setId(_id);
+		this.setBalance(_balance);	
+	}
+
+	//accessors and mutators
+	private int getId() {
+		return id;
+	}
+	private void setId(int id) {
+		this.id = id;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getAnualInterestRate() {
+		return anualInterestRate;
+	}	
+	public void setAnualInterestRate(double anualInterestRate) {
+		this.anualInterestRate = anualInterestRate;
+	}
+	
+	//computatins
+	public LocalDate getDate() {
+		return dateCreated;
+	}
+	public double getMonthelyInterestRate() {
+		return (this.anualInterestRate/100)/12; //converting annual interest rate to monthly
+	}
+	public double getMonthelyInterest() {
+		return getMonthelyInterestRate()*balance; //monthely interest you get on your balance
+	}
+	public void withdraw (double amount) {
+		if ((balance - amount) <=200) {
+			System.out.println("You must keep atleast 200$");
+		}
+		else
+			balance = balance - amount;
+	}
+	public void deposit(double amount) {
+		if(amount <=0) {
+			System.out.println("Your not depositing anything!");
+		}
+		else
+			balance = balance + amount;
+	}
+	
+	public String toString() {
+		return ""+ id + " "+ balance + " "+ anualInterestRate;
+	}
+	
+	public static void main(String[] args) {
+		Account account = new Account(112, 20000, 4.5);
+		account.withdraw(2500);
+		account.deposit(3000);
+		System.out.println(account);
+		
+		System.out.println(account.getBalance() + " " + account.getMonthelyInterest()+" "+ account.getDate());
+	}
+}
